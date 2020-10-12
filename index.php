c4aeb3fb2c95ea6acb2936c2deac5e28e32e8899
Lean Marai <lean@marai.com.ar>
2020-10-12 10:57:02 -0300

    agregardo formulario post

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 
 require_once "controladores/plantilla.controlador.php";
+require_once "controladores/formularios.controlador.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrTraerPlantilla();
--- vistas/paginas/registro.php
+++ vistas/paginas/registro.php
@@ -1,15 +1,44 @@
-<h1>Registro</h1>
-<form>
-	<div class="form-group">
-		<label for="email">Email:</label>
-		<input type="email" class="form-control" id="email">
-	</div>
-	<div class="form-group">
-		<label for="pwd">Password:</label>
-		<input type="password" class="form-control" id="pwd">
-	</div>
-	<div class="checkbox">
-		<label><input type="checkbox"> Recordarme</label>
-	</div>
-	<button type="submit" class="btn btn-outline-success">Enviar</button>
-</form>
+<div class="d-flex justify-content-center">
+	<form class="p-5 bg-light" method="post">
+
+		<div class="form-group">
+			<label for="nombre">Nombre:</label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fas fa-user"></i></span>
+				</div>
+				<input type="text" class="form-control" id="nombre" name="registroNombre">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="email">Email:</label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fas fa-envelope"></i></span>
+				</div>	
+				<input type="email" class="form-control" id="email" name="registroMail">
+			</div>
+
+		</div>
+
+		<div class="form-group">
+			<label for="pwe">Contraseña:</label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fas fa-lock"></i></span>
+				</div>	
+				<input type="pasword" class="form-control" id="pwd" name="registroPass">
+			</div>
+
+		</div>
+
+		<?php
+
+		$registro = new ControladorFormularios();
+		$registro -> ctrRegistro();
+
+		?>
+		<button type="submit" class="btn btn-outline-success btn-block">Enviar</button>
+	</form>
+</div>
